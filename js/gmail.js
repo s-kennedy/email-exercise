68b9ac50f796ad9a789428013bd048d72c13defa
Sharon <sharon.peishan.kennedy@gmail.com>
2015-01-30 18:01:08 +0100

    expand and reduce emails to show full text

--- js/gmail.js
+++ js/gmail.js
@@ -4,60 +4,69 @@ $(document).ready(function () {
 
 // check for new emails
 
-$("#check-emails").bind('click', checkEmails)
-function checkEmails(e) {
-	$.get(SERVER+'/email/new', function(data) {
-		console.log(data);
-		$("#number-new-emails").text('('+Object.keys(data).length+')'); // number of new emails next to "Inbox"
-		$.each( data, function( key, value ) {
-			$("#emails li:first").before(addEmail(key, value));
-		});	
-	});
-};
+	$("#check-emails").bind('click', checkEmails)
+	function checkEmails(e) {
+		$.get(SERVER+'/email/new', function(data) {
+			console.log(data);
+			$("#number-new-emails").text('('+Object.keys(data).length+')'); // number of new emails next to "Inbox"
+			$.each( data, function( key, value ) {
+				$("#emails li:first").before(addEmail(key, value));
+			});	
+		});
+	};
 
-// add new emails to the list of emails
+	// add new emails to the list of emails
 
-function addEmail(id, email){
-	return '<li data-id="'+ id +'">'+
-          '<div class="checked" data-checked="false"></div>'+
-              '<div class="starred" data-starred="'+email.started+'"></div>'+
-              '<div class="from">'+email.from+'</div>'+
-              '<div class="subject">'+email.subject+'</div>'+
-              '<div class="preview">'+email.preview+'</div>'+
-            '</li>';
-};
- 
+	function addEmail(id, email){
+		return '<li data-id="'+ id +'">'+
+	          '<div class="checked" data-checked="false"></div>'+
+	              '<div class="starred" data-starred="'+email.started+'"></div>'+
+	              '<div class="from">'+email.from+'</div>'+
+	              '<div class="subject">'+email.subject+'</div>'+
+	              '<div class="preview">'+email.preview+'</div>'+
+	            '</li>';
+	};
+	 
 
-// get full text of email
+	// get full text of email
 
-$("#emails").delegate("li .subject", "click", fullEmail)
+	$("#emails").delegate("li", "click", fullEmail)
 
-function fullEmail(e) {
-	var emailId = $(this).parent().data("id");
-	$.get(SERVER+'/email/'+emailId, function(data) {
-		var emailText = data.email;
-		$("li[data-id='"+emailId+"'] .preview").html(emailText);
-	});
-};
+	function fullEmail(e) {
+		var emailId = $(this).data("id");
+		var self = this;
+
+		$.get(SERVER+'/email/'+emailId, function(data) {
+			var emailText = data.email;
+			var emailPreview = data.preview;
+			$("li[data-id='"+emailId+"'] .preview").html(emailText);
+		});
+			$(self).children(".preview").click(function() {
+				// e.stopPropagation();
+				// console.log($(self).children(".preview"));
+				$(self).children(".preview").text("test");
+				return false;
+			});
+	};
 
 
-// send email
+	// send email
 
-$("#send").bind('click', sendEmail)
+	$("#send").bind('click', sendEmail)
 
-function sent() {
-	alert("Message sent!")
-}
+	function sent() {
+		alert("Message sent!")
+	}
 
-function sendEmail(e) {
-	$.ajax({
-	type: "post",
-	url: SERVER+'/email',
-	data: $("#blank-to").val(),
-	success: sent
-});
-	$('#compose-email').modal('hide')
+	function sendEmail(e) {
+		$.ajax({
+			type: "post",
+			url: SERVER+'/email',
+			data: $("#blank-to").val(),
+			success: sent
+		});
+		$('#compose-email').modal('hide')
 
-};
+	};
 
 })
