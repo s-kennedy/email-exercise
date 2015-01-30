aabf65fd3b3f3397dd1440f2d2f9416b2ac04484
Sharon <sharon.peishan.kennedy@gmail.com>
2015-01-30 15:52:49 +0100

    functions for checking for new emails and retrieving full email text

--- js/gmail.js
+++ js/gmail.js
@@ -2,20 +2,46 @@ $(document).ready(function () {
 
     var SERVER = 'https://vast-earth-2490.herokuapp.com';
 
+// check for new emails
+
 $("#check-emails").bind('click', checkEmails)
 function checkEmails(e) {
 	$.get(SERVER+'/email/new', function(data) {
-		console.log(data)
-		$("#number-new-emails").text('('+Object.keys(data).length+')');
+		console.log(data);
+		$("#number-new-emails").text('('+Object.keys(data).length+')'); // number of new emails next to "Inbox"
 		$.each( data, function( key, value ) {
-		alert( key + ": " + value );
-	});
-		$("#emails li:first").before('<li>hey</li>');
+			$("#emails li:first").before(addEmail(key, value));
+		});	
 	});
 };
 
-// $("#emails li .subject").click(function() {
-// 	$(this)
-// })
+// add new emails to the list of emails
+
+function addEmail(id, email){
+	return '<li data-id="'+ id +'">'+
+          '<div class="checked" data-checked="false"></div>'+
+              '<div class="starred" data-starred="'+email.started+'"></div>'+
+              '<div class="from">'+email.from+'</div>'+
+              '<div class="subject">'+email.subject+'</div>'+
+              '<div class="preview">'+email.preview+'</div>'+
+            '</li>';
+};
+ 
+
+// get full text of email
+
+$("#emails").delegate("li .subject", "click", fullEmail)
+
+function fullEmail(e) {
+	var emailId = $(this).parent().data("id");
+	var self = this;
+	$.get(SERVER+'/email/'+emailId, function(data) {
+		var emailText = data.email;
+		$("li[data-id='"+emailId+"'] .preview").html(emailText);
+	});
+};
+
+
+// 
 
 })
