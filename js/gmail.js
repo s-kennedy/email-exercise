7f97aa9c8b9584e43ef4e3021b91fc2a178948f0
Sharon <sharon.peishan.kennedy@gmail.com>
2015-02-02 09:43:33 +0100

    starring important emails

--- js/gmail.js
+++ js/gmail.js
@@ -20,10 +20,10 @@ $(document).ready(function () {
 	function addEmail(id, email){
 		return '<li data-id="'+ id +'">'+
 	          '<div class="checked" data-checked="false"></div>'+
-	              '<div class="starred" data-starred="'+email.started+'"></div>'+
+	              '<div class="starred" data-starred="'+email.started+'"><img src="images/star4.png" alt="star0" class="star0"><img src="images/star-lit4.png" alt="star1" class="star1"</div>'+
 	              '<div class="from">'+email.from+'</div>'+
 	              '<div class="subject">'+email.subject+'</div>'+
-	              '<div class="preview">'+email.preview+'</div>'+
+	              '<div class="preview">'+email.preview+'</div>'+'<div class="fulltext"></div>'+
 	            '</li>';
 	};
 	 
@@ -34,15 +34,17 @@ $(document).ready(function () {
 
 	function fullEmail(e) {
 		var emailId = $(this).data("id");
-		var self = this;
 
 		$.get(SERVER+'/email/'+emailId, function(data) {
 			var emailText = data.email;
-			var emailPreview = data.preview;
-			$("li[data-id='"+emailId+"'] .preview").html(emailText);
+
+			$("li[data-id='"+emailId+"'] .fulltext").html(emailText);
+			$("li[data-id='"+emailId+"'] .preview").hide()
+			
 		});
-			$(self).children(".preview").click(function() {
-				$(self).children(".preview").text("test");
+			$("li[data-id='"+emailId+"'] .fulltext").click(function() {
+				$("li[data-id='"+emailId+"'] .fulltext").hide()
+				$("li[data-id='"+emailId+"'] .preview").show()
 				return false;
 			});
 	};
@@ -60,11 +62,42 @@ $(document).ready(function () {
 		$.ajax({
 			type: "post",
 			url: SERVER+'/email',
-			data: $("#blank-to").val(),
+			data: $("#blank-to", "#blank-content").val(),
 			success: sent
 		});
 		$('#compose-email').modal('hide')
 
 	};
+
+// make stars visible only when starred is true
+
+	// $("#emails").delegate("li div.starred").each(function() {
+
+	$("#emails li div.starred").each(function() {
+		if ($(this).data("starred") == true) {
+			$(this).find("img.star0").hide()
+		} else {
+			$(this).find("img.star1").hide()
+		}
+
+		});
+
+// clicking on stars changes the color
+
+	$("img.star0").bind('click', function() {
+		$(this).hide();
+		$(this).parent().find("img.star1").show();
+		// $(this).parent().data("starred") = true;
+	});
+
+	$("img.star1").bind('click', function() {
+		$(this).hide();
+		$(this).parent().find("img.star0").show();
+		// $(this).parent().data("starred") = false;
+	});
+
+// archive an email to hide it
+
+
 
 })
